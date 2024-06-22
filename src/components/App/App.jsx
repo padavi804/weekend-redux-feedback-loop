ff09026274e47f1bc2aabc559ab5889373354a61
Patrick Davison <davisonpatrickj@gmail.com>
2024-06-22 15:54:18 -0500

    Add component folders

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
 
   return (
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+
+
+
+
+      
     </div>
+    </Router>
   );
 }
 
